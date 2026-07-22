7dd1612f15f4deb7af72382beccc482a58f6bd2e
solomon <ssbothwell@gmail.com>
2026-07-22 15:02:04 -0700

    docs: fix ambiguous / out-of-scope Haddock identifier links

--- src/Kindly/Functor.hs
+++ src/Kindly/Functor.hs
@@ -70,7 +70,7 @@ fmap = map1
 -- in 'Data.Functor.Contravariant.'
 --
 -- TODO: Do we keep this around? This is nice to have so that library
--- users don't have to manually pack functions in 'Op'.
+-- users don't have to manually pack functions in t'Op'.
 contramap :: (Functor Op p) => (a -> b) -> p b -> p a
 contramap = fmap . Op
 
@@ -78,7 +78,7 @@ contramap = fmap . Op
 -- in 'Data.Functor.Invariant.'
 --
 -- TODO: Do we keep this around? This is nice to have so that library
--- users don't have to manually pack functions in 'Iso'.
+-- users don't have to manually pack functions in t'Iso'.
 invmap :: (Functor (Iso (->)) f) => (a -> b) -> (b -> a) -> f a -> f b
 invmap f g = fmap (Iso f g)
 
@@ -88,7 +88,7 @@ invmap f g = fmap (Iso f g)
 type Filterable p = Functor (Hask.Profunctor.Star Maybe) p
 
 -- | A specialization of 'fmap' for filterable functors as defined
--- in 'Witherable'
+-- in "Witherable"
 --
 -- TODO: Do we keep this around? This is nice to have so that library
 -- users don't have to manually pack functions in 'Hask.Star'.
